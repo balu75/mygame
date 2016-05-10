7b3fbfdd0bafec383a99089079148d096bec143a
Thomas <thomas.gies@gmx.de>
2016-05-10 22:45:16 +0200

    licenses

--- settings.py
+++ settings.py
@@ -1,5 +1,20 @@
-# field dimensions
+# Copyright (C) 2016
+# Author: Thomas Gies <thomas.gies@gmx.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# field dimensions
 global max_x
 max_x = 1200
 
